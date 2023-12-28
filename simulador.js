bc545a6c457334f77efe4f04554ea0032bea9a70
matimarino <matiasn.marino03@gmail.com>
2023-12-28 18:09:04 -0300

    proyecto final

--- simulador.js
+++ simulador.js
@@ -1,88 +1,91 @@
-function calcularCotizacion() {
-    const tipoSeguro = document.getElementById("tipoSeguro").value;
-    const edad = parseInt(document.getElementById("edad").value);
-    const cobertura = document.getElementById("cobertura").value;
-    const extras = obtenerExtrasSeleccionados();
+async function calcularCotizacion() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        const data = await response.json();
 
-    let costoSeguroBase = 0;
+        const { userId, title, completed } = data;
 
-    switch (tipoSeguro) {
-        case "auto":
-            costoSeguroBase = 500;
-            break;
-        case "hogar":
-            costoSeguroBase = 700;
-            break;
-        case "vida":
-            costoSeguroBase = 300;
-            break;
-        case "salud":
-            costoSeguroBase = 600;
-            break;
-        case "viaje":
-            costoSeguroBase = 200;
-            break;
-        case "celular":
-            costoSeguroBase = 100;
-            break;
-        default:
-            costoSeguroBase = 0;
-            break;
-    }
+        let costoSeguroBase = obtenerCostoBase(document.getElementById("tipoSeguro").value);
 
-    let costoExtras = calcularCostoExtras(extras);
+        let costoExtras = calcularCostoExtras(["robo", "danios", "grua"]);
 
-    if (edad < 25) {
-        costoSeguroBase += costoSeguroBase * 0.2; 
-    }
-
-    switch (cobertura) {
-        case "intermedia":
-            costoSeguroBase += costoSeguroBase * 0.15; 
-            break;
-        case "completa":
-            costoSeguroBase += costoSeguroBase * 0.3; 
-            break;
-        default:
-            break;
-    }
-
-    const costoTotal = costoSeguroBase + costoExtras;
-
-    document.getElementById("costoSeguro").textContent = `$${costoTotal.toFixed(2)}`;
-}
-
-function obtenerExtrasSeleccionados() {
-    const extras = document.getElementsByName("extras");
-    const extrasSeleccionados = [];
-
-    for (const extra of extras) {
-        if (extra.checked) {
-            extrasSeleccionados.push(extra.value);
+        if (edad < 25) {
+            costoSeguroBase += costoSeguroBase * 0.2;
         }
-    }
 
-    return extrasSeleccionados;
-}
-
-function calcularCostoExtras(extras) {
-    let costoExtras = 0;
-
-    for (const extra of extras) {
-        switch (extra) {
-            case "robo":
-                costoExtras += 50;
+        switch (document.getElementById("cobertura").value) {
+            case "intermedia":
+                costoSeguroBase += costoSeguroBase * 0.15;
                 break;
-            case "danios":
-                costoExtras += 80;
-                break;
-            case "grua":
-                costoExtras += 30;
+            case "completa":
+                costoSeguroBase += costoSeguroBase * 0.3;
                 break;
             default:
                 break;
         }
+
+        const costoTotal = costoSeguroBase + costoExtras;
+
+        document.getElementById("costoSeguro").textContent = `$${costoTotal.toFixed(2)}`;
+
+        localStorage.setItem('userId', userId);
+        localStorage.setItem('title', title);
+        localStorage.setItem('completed', completed);
+
+        Swal.fire('¡Cálculo exitoso!', 'La cotización se ha calculado correctamente.', 'success');
+
+    } catch (error) {
+        console.error('Error al obtener datos:', error);
+        Swal.fire('Error', 'Hubo un problema al obtener los datos. Por favor, inténtalo de nuevo.', 'error');
+    }
+}
+
+function obtenerCostoBase(tipoSeguro) {
+    switch (tipoSeguro) {
+        case "auto":
+            return 500;
+        case "hogar":
+            return 700;
+        case "vida":
+            return 300;
+        case "salud":
+            return 600;
+        case "viaje":
+            return 200;
+        case "celular":
+            return 100;
+        default:
+            return 0;
+    }
+}
+
+function calcularCostoExtras(extras) {
+    const costoPorExtra = {
+        robo: 50,
+        danios: 80,
+        grua: 30
+    };
+
+    let costoExtras = 0;
+
+    for (const extra of extras) {
+        costoExtras += costoPorExtra[extra] || 0;
     }
 
     return costoExtras;
 }
+
+document.getElementById("calcularBtn").addEventListener("click", async () => {
+    if (validarFormulario()) {
+        await calcularCotizacion();
+    } else {
+        alert('Por favor, ingresa una edad válida.');
+    }
+});
+
+const validarFormulario = () => {
+    const edad = parseInt(document.getElementById("edad").value);
+    return !isNaN(edad) && edad > 0;
+};
+
+calcularCotizacion();
